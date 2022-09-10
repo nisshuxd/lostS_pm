56904503bb753af18dfbf1021b18a83d1855df48
Rayan Kumaranga <94668516+rayanomk@users.noreply.github.com>
2022-09-10 10:49:19 +0530

    Add files via upload

---
+++ runLinux.sh
@@ -0,0 +1,6 @@
+@echo off
+set MODNAME=lostS_pm
+set MAPNAME=mp_killhouse
+
+cd ..\..\
+cod4x18_dedrun +set sv_punkbuster 0 +set pb_sv_enable 0 +map_rotate +sv_authorizemode 0 +set fs_game Mods/%MODNAME% +map %MODNAME% +set r_xassetnum "material=2560 xmodel=1200 xanim=3200 image=3000" +set promod_mode custom_public + exec server.cfg +set g_gametype sd
